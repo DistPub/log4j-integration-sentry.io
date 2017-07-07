7e9e186a1a850adbc2e4059b769d7d335a3ff82c
Smite Chow <xiaopengyou2no1@gmail.com>
2017-07-07 19:48:30 +0800

    use file logger

--- src/main/java/com/smitechow/test/App.java
+++ src/main/java/com/smitechow/test/App.java
@@ -2,17 +2,25 @@ package com.smitechow.test;
 
 import org.apache.log4j.Logger;
 
-/**
- * Hello world!
- *
- */
-public class App 
+import java.util.concurrent.TimeUnit;
+
+public class App
 {
     final static Logger logger = Logger.getLogger(App.class);
 
-    public static void main( String[] args )
-    {
-        logger.error("This is error : test");
+    public static void main( String[] args ) throws InterruptedException {
+        logger.trace("trace hello sentry");
         System.out.println( "Hello World!" );
+        logger.fatal("fatal hello sentry");
+        System.out.println( "Hello World!" );
+        logger.error("error hello sentry");
+        System.out.println( "Hello World!" );
+        logger.warn("warn hello sentry");
+        System.out.println( "Hello World!" );
+        logger.info("info hello sentry");
+        System.out.println( "Hello World!" );
+        logger.debug("debug hello sentry");
+        System.out.println( "Hello World!" );
+        TimeUnit.MINUTES.sleep(60);
     }
 }
